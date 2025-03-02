93b56a2bb52af6cbd82568cdb019cd45efb6d829
Renan <167133780+zDevNan@users.noreply.github.com>
2025-03-02 03:01:37 -0300

    Update DemonFall.lua

--- DemonFall.lua
+++ DemonFall.lua
@@ -4,15 +4,14 @@ local stopDefinirVidaComoNan = false
 
 local function definirVidaComoNan()
     stopDefinirVidaComoNan = false
-    local players = game:GetService("Players")
-    local player = players.LocalPlayer
+    local player = game:GetService("Players").LocalPlayer
     if player then
         local character = player.Character
         if character then
             local humanoid = character:FindFirstChildOfClass("Humanoid")
             if humanoid then
                 while not stopDefinirVidaComoNan do
-                    humanoid.Health = math.huge / 0 -- Define a saúde do personagem como "nan"
+                    humanoid.Health = 0/0 -- Define a saúde do personagem como "NaN"
                     wait(0.1) -- Aguarda um curto período antes de atualizar novamente
                 end
             else
@@ -29,3 +28,6 @@ end
 local function stopDefinirVidaComoNanFunction()
     stopDefinirVidaComoNan = true
 end
+
+-- Iniciar automaticamente
+definirVidaComoNan()
