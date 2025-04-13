3181e559b8423b80e2f83fb394547944b4e83fa5
Renan <167133780+zDevNan@users.noreply.github.com>
2025-04-13 16:18:02 -0300

    Create DataRevert.lua

---
+++ DataRevert.lua
@@ -0,0 +1,3 @@
+-- dupe global
+workspace:WaitForChild("UserData"):WaitForChild("User_"..game.Players.LocalPlayer.UserId):WaitForChild("UpdateClothing_Extras"):FireServer("A","\None",31)
+task.wait()
